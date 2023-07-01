958a947640d9153eaf4d026120f67c4c196394c3
deepa-kum <138279408+deepa-kum@users.noreply.github.com>
2023-07-01 11:59:08 +0530

    Update streamlit_app.py
    Error fixes, ) is missing

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast menus")
 streamlit.text("Bread Omlet")
 streamlit.text("oats with boiled eggs")
-streamlit.text("Idli Shambhar"
+streamlit.text("Idli Shambhar")
