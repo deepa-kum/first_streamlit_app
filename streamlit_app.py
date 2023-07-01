00b97b6e259867b2695d0d04e80502572f5ba87e
deepa-kum <138279408+deepa-kum@users.noreply.github.com>
2023-07-01 11:58:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast menus")
 streamlit.text("Bread Omlet")
-streamlit.text("oats with boiled eggs)
+streamlit.text("oats with boiled eggs")
 streamlit.text("Idli Shambhar"
