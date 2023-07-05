923859bdc9f4b70c20f4df142ce0cdefdea47469
deepa-kum <138279408+deepa-kum@users.noreply.github.com>
2023-07-05 12:53:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ streamlit.write('The user entered ', fruit_choice)
 import snowflake.connector
 
 #####adding the below connector 
+#########made the secrets changes
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
