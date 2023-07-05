cea31e3186b9ecbbb7f1fedad3d0765afe04950c
deepa-kum <138279408+deepa-kum@users.noreply.github.com>
2023-07-05 12:56:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,10 +39,11 @@ import snowflake.connector
 
 #####adding the below connector 
 #########made the secrets changes
+########query some data
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * from fruit__load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
